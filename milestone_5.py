72b4ca01dfe92d791f89258267552f1099931f7c
japi07 <azpiazujavier@gmail.com>
2024-06-13 11:31:56 +0200

    Add files via upload

---
+++ milestone_5.py
@@ -0,0 +1,60 @@
+#%%
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        """Check if the guessed letter is in the secret word."""
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        """Prompt the user for input until a valid guess is provided."""
+        while True:
+            guess = input("Guess a letter: ")
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+def play_game(word_list):
+    """Run the Hangman game with the given word list."""
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters > 0:
+            print(f"Word guessed so far: {' '.join(game.word_guessed)}")
+            game.ask_for_input()
+        else:
+            print("Congratulations. You won the game!")
+            break
+# Example usage
+if __name__ == "__main__":
+    word_list = ["ale", "gusy", "gusanito", "gusygumer"]
+    play_game(word_list)
+#%%
+
